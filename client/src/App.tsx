5dbfe8edf8f37f9a123045fc9c46c114ccde9299
Luna <Luna.dev.97@gmail.com>
2026-02-13 14:47:38 -0500

    feat: gh-pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base="/cass_valentine">
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
